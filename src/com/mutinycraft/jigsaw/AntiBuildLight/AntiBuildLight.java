44e0ed21d9f991994b86705c14c972483f50ad4f
MutinyCraft <admin@mutinycraft.com>
2014-05-24 21:02:35 -0500

    Remove unneeded stuff.

--- src/com/mutinycraft/jigsaw/AntiBuildLight/AntiBuildLight.java
+++ src/com/mutinycraft/jigsaw/AntiBuildLight/AntiBuildLight.java
@@ -4,21 +4,12 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.logging.Logger;
-
 public class AntiBuildLight extends JavaPlugin implements Listener {
-
-    Logger log;
-    private static final String VERSION = " v1.1";
-    private static final String MESSAGE = "&cIf you wish to build, visit: &djoin.mutinycraft.com";
+    private static final String MESSAGE = "&cYou are not allowed to build!";
 
     public void onEnable() {
-        log = this.getLogger();
-
         getServer().getPluginManager().registerEvents(this, this);
         new AntiBuildEventHandler(this);
-
-        log.info(this.getName() + VERSION + " enabled!");
     }
 
     /**
@@ -28,9 +19,5 @@ public class AntiBuildLight extends JavaPlugin implements Listener {
      */
     public String getMessage() {
         return ChatColor.translateAlternateColorCodes('&', MESSAGE);
-    }
-
-    public void onDispable() {
-        log.info(this.getName() + VERSION + " disabled!");
     }
 }
